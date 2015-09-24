49c9fe0f0631bb3b50251637fd9c96b6aeb0f49a
Carlovan <giulio.carlassare@gmail.com>
2015-09-24 18:01:36 +0200

    Initial commit
    HTTPListener test

---
+++ Poker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net;
+
+namespace Poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!HttpListener.IsSupported)
+            {
+                Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+                return;
+            }
+
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8080/");
+            listener.Prefixes.Add("http://192.168.1.3:8081/");
+            listener.Start();
+            Console.WriteLine("Listening...");
+
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerResponse response = context.Response;
+            HttpListenerRequest request = context.Request;
+            string s = request.RemoteEndPoint.Address.MapToIPv4().ToString();
+
+            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+            listener.Stop();
+
+            Console.WriteLine("Richiesta da {0}", s);
+
+            Console.ReadLine();
+        }
+    }
+}
